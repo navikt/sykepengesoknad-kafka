2a5122d30480d6ec3c76d2323b3c91109de5fbd6
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2019-11-25 13:24:29 +0100

    CallId håndtering
    Sjekk for tom streng nå callid genereres.
    Gjør gammel konstant internal til modulen
    Trekker utilsfunksjoner ut i egen fil

--- kafkautils/src/main/kotlin/no/nav/syfo/kafka/KafkaHeaderConstants.kt
+++ kafkautils/src/main/kotlin/no/nav/syfo/kafka/KafkaHeaderConstants.kt
@@ -1,17 +1,9 @@
 package no.nav.syfo.kafka
 
-import org.apache.kafka.common.header.Headers
-import java.nio.charset.StandardCharsets.UTF_8
-
 const val GUID = "guid"
 const val TYPE = "type"
 const val CREATED_DATE = "createdDate"
-const val CALL_ID = "callId"
 const val NAV_CALLID = "Nav-Callid"
 const val MELDINGSTYPE = "MELDINGSTYPE"
+internal const val CALL_ID = "callId"
 
-fun getLastHeaderByKeyAsString(headers: Headers?, key: String): String?  =
-        headers
-                ?.lastHeader(key)
-                ?.value()
-                ?.let { String(it, UTF_8) }
--- kafkautils/src/main/kotlin/no/nav/syfo/kafka/SyfoProducerRecord.kt
+++ kafkautils/src/main/kotlin/no/nav/syfo/kafka/SyfoProducerRecord.kt
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets.UTF_8
 import java.time.LocalDateTime
 import java.util.*
 import java.util.Collections.emptyMap
+import kotlin.collections.ArrayList
 
 class SyfoProducerRecord<K, V>(topic: String, key: K, value: V, headers: Map<String, Any> = emptyMap()) :
         ProducerRecord<K, V>(topic, null, System.currentTimeMillis(), key, value, defaultHeaders<V>(value, headers)) {
@@ -21,15 +22,20 @@ class SyfoProducerRecord<K, V>(topic: String, key: K, value: V, headers: Map<Str
     companion object {
         private fun <V> defaultHeaders(value: V, additionalHeaders: Map<String, Any>): Iterable<Header> {
             val guid = RecordHeader(GUID, toUtf8Bytes(UUID.randomUUID()))
-            val callId = RecordHeader(CALL_ID, toUtf8Bytes(MDC.get(NAV_CALLID)))
-            val navCallid = RecordHeader(NAV_CALLID, toUtf8Bytes(MDC.get(NAV_CALLID)))
+            val callId = Optional.ofNullable(MDC.get(NAV_CALLID))
+                    .filter(String::isNotEmpty)
+                    .filter(String::isNotBlank)
+                    .orElse(UUID.randomUUID().toString())
+
+            val callIdHeader = RecordHeader(CALL_ID, toUtf8Bytes(callId))
+            val navCallidHeader = RecordHeader(NAV_CALLID, toUtf8Bytes(callId))
             val type = RecordHeader(TYPE, toUtf8Bytes((value as? Any)?.javaClass?.simpleName))
             val createdDate = RecordHeader(CREATED_DATE, toUtf8Bytes(LocalDateTime.now()))
 
             return ArrayList<Header>().apply {
                 add(guid)
-                add(callId)
-                add(navCallid)
+                add(callIdHeader)
+                add(navCallidHeader)
                 add(type)
                 add(createdDate)
                 addAll(additionalHeaders.entries.map { entry -> RecordHeader(entry.key, toUtf8Bytes(entry.value)) })
