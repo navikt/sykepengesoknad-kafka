53dc2d73987eaf656e5c080a2fafcb090140f7d4
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-01-11 14:09:36 +0100

    Legg til arbeidssituasjon Fisker og Jordbruker

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/ArbeidssituasjonDTO.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/ArbeidssituasjonDTO.kt
@@ -2,6 +2,8 @@ package no.nav.helse.flex.sykepengesoknad.kafka
 
 enum class ArbeidssituasjonDTO {
     SELVSTENDIG_NARINGSDRIVENDE,
+    FISKER,
+    JORDBRUKER,
     FRILANSER,
     ARBEIDSTAKER,
     ARBEIDSLEDIG,
