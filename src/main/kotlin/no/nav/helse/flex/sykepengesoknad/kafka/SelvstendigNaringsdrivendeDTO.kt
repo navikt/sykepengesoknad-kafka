5e00417f012a847a259577cede8de9fb42019c56
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2025-02-19 16:24:29 +0100

    sigrun data for næringsdrivende (#103)
    Co-authored-by: Eirik HS <eirik.skjaerseth@gmail.com>

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
@@ -1,10 +1,53 @@
 package no.nav.helse.flex.sykepengesoknad.kafka
 
+import java.math.BigInteger
+
 data class SelvstendigNaringsdrivendeDTO(
-    val roller: List<Rolle>,
+    val roller: List<RolleDTO>,
+    val sykepengegrunnlagNaeringsdrivende: SykepengegrunnlagNaeringsdrivendeDTO? = null,
 )
 
-data class Rolle(
+data class AarVerdiDTO(
+    val aar: String,
+    val verdi: BigInteger,
+)
+
+data class BeregnetDTO(
+    val snitt: BigInteger,
+    val p25: BigInteger,
+    val m25: BigInteger,
+)
+
+data class SykepengegrunnlagNaeringsdrivendeDTO(
+    val gjennomsnittPerAar: List<AarVerdiDTO>,
+    val grunnbeloepPerAar: List<AarVerdiDTO>,
+    val grunnbeloepPaaSykmeldingstidspunkt: Int,
+    val beregnetSnittOgEndring25: BeregnetDTO,
+    val inntekter: List<HentPensjonsgivendeInntektResponseDTO>,
+)
+
+data class HentPensjonsgivendeInntektResponseDTO(
+    val norskPersonidentifikator: String,
+    val inntektsaar: String,
+    val pensjonsgivendeInntekt: List<PensjonsgivendeInntektDTO>,
+)
+
+data class PensjonsgivendeInntektDTO(
+    val datoForFastsetting: String,
+    val skatteordning: SkatteordningDTO,
+    val pensjonsgivendeInntektAvLoennsinntekt: Int = 0,
+    val pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel: Int = 0,
+    val pensjonsgivendeInntektAvNaeringsinntekt: Int = 0,
+    val pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage: Int = 0,
+)
+
+enum class SkatteordningDTO {
+    FASTLAND,
+    SVALBARD,
+    KILDESKATT_PAA_LOENN,
+}
+
+data class RolleDTO(
     val orgnummer: String,
     val rolletype: String,
 )
