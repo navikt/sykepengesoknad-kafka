4b0d3729e93c297e45f41fc57f7c98225b4c5cf3
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-05-20 07:43:32 +0200

    Legg til nytt felt for inntekt for næringsdrivende (#117)

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
@@ -4,7 +4,30 @@ import java.math.BigInteger
 
 data class SelvstendigNaringsdrivendeDTO(
     val roller: List<RolleDTO>,
+    @Deprecated(
+        message = "Fjernes når alle konsumenter har tatt i bruk NaringsdrivendeInntektDTO.",
+        replaceWith = ReplaceWith("naringsdrivendeInntekt"),
+    )
     val sykepengegrunnlagNaeringsdrivende: SykepengegrunnlagNaeringsdrivendeDTO? = null,
+    val naringsdrivendeInntekt: NaringsdrivendeInntektDTO? = null,
+)
+
+data class NaringsdrivendeInntektDTO(
+    val norskPersonidentifikator: String,
+    val inntekt: List<NaringsdrivendeInntektsAarDTO>,
+)
+
+data class NaringsdrivendeInntektsAarDTO(
+    val inntektsaar: String,
+    val pensjonsgivendeInntekt: SummertPensjonsgivendeInntektDTO,
+)
+
+// Inneholder summert verdier for skatteordningene SVALBARD og FASTLAND.
+data class SummertPensjonsgivendeInntektDTO(
+    val pensjonsgivendeInntektAvLoennsinntekt: Int? = 0,
+    val pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel: Int? = 0,
+    val pensjonsgivendeInntektAvNaeringsinntekt: Int? = 0,
+    val pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage: Int? = 0,
 )
 
 data class AarVerdiDTO(
