9be5a4bd9889a81fe273241638fdc66064f7263b
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-11-18 10:40:43 +0100

    Dokumentasjon av SoknadsperiodeDTO (#159)

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SoknadsperiodeDTO.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SoknadsperiodeDTO.kt
@@ -3,11 +3,30 @@ package no.nav.helse.flex.sykepengesoknad.kafka
 import java.time.LocalDate
 
 data class SoknadsperiodeDTO(
+    /**
+     * Første dag i sykmeldingsperioden.
+     */
     val fom: LocalDate? = null,
+    /**
+     * Siste dag i sykmeldingsperioden.
+     */
     val tom: LocalDate? = null,
+    /**
+     * Sykmeldingsgrad fra sykmeldingen. Sammenfaller med at sykmeldingstype er
+     * SykmeldingstypeDTO.GRADERT hvis graderingen er mindre enn 100 %.
+     */
     val sykmeldingsgrad: Int? = null,
+    /**
+     * Hvor mange prosent av avtalte timer per uke det ble jobbet.
+     */
     val faktiskGrad: Int? = null,
+    /**
+     * Normal (avtalte) antall arbeidstimer per uke.
+     */
     val avtaltTimer: Double? = null,
+    /**
+     * Timer faktisk jobbet i sykmeldingsperioden.
+     */
     val faktiskTimer: Double? = null,
     val sykmeldingstype: SykmeldingstypeDTO? = null,
     // Fra gammel SoknadDTO. Bør konsolideres med sykmeldingsgrad.
