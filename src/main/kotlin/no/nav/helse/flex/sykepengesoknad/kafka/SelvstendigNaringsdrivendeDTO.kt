4f7d1b04f3a147589600bc7ba97441aa1bbaae77
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-09-02 13:03:29 +0200

    endrer navn likt sykepengesoknad-backend

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/kafka/SelvstendigNaringsdrivendeDTO.kt
@@ -16,7 +16,7 @@ data class InntektsAarDTO(
     val aar: String,
     val pensjonsgivendeInntekt: PensjonsgivendeInntektDTO?,
     val erFerdigLignet: Boolean = pensjonsgivendeInntekt != null,
-    val datoForFastsettelse: String? = null,
+    val datoForFastsetting: String? = null,
 )
 
 /**
